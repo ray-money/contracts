ff2d89ba5a39c8a34a0226ac1f815199646e2024
kei at <arseneeth@pm.me>
2024-11-26 08:11:40 +0100

    add more files, enable eth withdrawal, add comments

--- src/vault/LTVManager.sol
+++ src/vault/LTVManager.sol
@@ -5,7 +5,4 @@ contract CoverToken {
 
     constructor() {}
 
-    // TODO: mint token
-    // TODO: burn token
-    // TODO: set permissions
 }
--- src/vault/Vault.sol
+++ src/vault/Vault.sol
@@ -2,26 +2,55 @@
 pragma solidity ^0.8.28;
 
 contract Vault {
-    // a list of whitelisted lsts
-    address[] public lsts;
-    
-    constructor(address[] memory _lsts) {
-        lsts = _lsts;
-    }
-    // Mapping to track user deposits
+    // mapping of whitelisted LSTs
+    mapping(address => bool) public whitelistedLsts;
+    //@dev Mapping to track user deposits
     mapping(address => uint256) public lps;
+    //@dev oracle address
+    address public oracle;
 
-    // deposit ETH to the vault
+    constructor(address[] memory _lsts, address _oracle) {
+        for(uint i = 0; i < _lsts.length; i++) {
+            whitelistedLsts[_lsts[i]] = true;
+        }
+        oracle = _oracle;
+    }
+
+    //@dev deposit ETH to the vault
     function depositETH(uint256 amount) public payable {
         require(msg.value == amount, "Amount mismatch");
         lps[msg.sender] += amount;
     }
 
-    // Prevent direct ETH transfers
+    //@dev withdraw ETH from the vault
+    function withdrawETH(uint256 amount) public {
+        require(lps[msg.sender] >= amount, "Insufficient balance");
+        require(address(this).balance >= amount, "Insufficient contract balance");
+        
+        lps[msg.sender] -= amount;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "ETH transfer failed");
+    }
+
+    // TODO: deposit LST from the list
+
+    // TODO: withdraw LST from the contract
+
+    // TODO: check the balance of ETH in the contract
+
+    // TODO: check the balance of a speciefic LST in the contract
+
+    //@dev Check if LST is whitelisted
+    function isWhitelistedLst(address lst) public view returns (bool) {
+        return whitelistedLsts[lst];
+    }
+
+    //@dev Prevent direct ETH transfers
     receive() external payable {
         revert("Direct ETH transfers not allowed");
     }
-    // Prevent direct ETH transfers with fallback
+
+    //@dev Prevent direct ETH transfers with fallback
     fallback() external payable {
         revert("Direct ETH transfers not allowed"); 
     }
