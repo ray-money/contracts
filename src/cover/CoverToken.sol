190568b59bf1b6276ddc1fab2dc931fc161d089d
kei at <arseneeth@pm.me>
2024-11-28 16:11:47 +0100

    add openzeppelin-contracts-upgradable, make TokenFactory cloning new tokens

--- src/cover/CoverToken.sol
+++ src/cover/CoverToken.sol
@@ -1,37 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import {ILTVManager} from "../interfaces/ILTVManager.sol";
 
-contract CoverToken is ERC20, Ownable {
+contract CoverToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     address public ltvManager;
+    bool private initialized;
 
     /** 
-     * @dev We should figure out naming for LRT/ETH specific cover tokens
+     * @dev Initialization function for the cloned contract
      * @param _owner - the owner of the contract, could be Controller.sol
      * @param _ltvManager - the LTVManager.sol address
+     * @param _name - the name of the cover token
+     * @param _symbol - the symbol of the cover token
      */
-    constructor(address _owner, address _ltvManager) ERC20("Cover Token", "COVER") Ownable(_owner) {
+    function initialize(
+        address _owner,
+        address _ltvManager,
+        string memory _name,
+        string memory _symbol
+    ) external {
+        require(!initialized, "Already initialized");
+        require(_owner != address(0), "Invalid owner");
+        require(_ltvManager != address(0), "Invalid LTV manager");
+        require(bytes(_name).length > 0, "Invalid name");
+        require(bytes(_symbol).length > 0, "Invalid symbol");
+        
+        __ERC20_init(_name, _symbol);
+        _transferOwnership(_owner);
         ltvManager = _ltvManager;
+        initialized = true;
     }
 
     /**
      * @dev Mints tokens to an address. Can only be called by owner.
      * @param to The address that will receive the minted tokens
      * @param token The address of the LRT token, or address(0) for ETH
-     * @dev Uses LTVManager to calculate mint amount based on token type
+     * @param amount The amount of tokens to mint
+     * @dev Uses LTVManager to check if amount is within allowed LTV limits
      */
-    function mint(address to, address token) external onlyOwner {
-        uint256 amount;
+    function mint(address to, address token, uint256 amount) external onlyOwner {
+        uint256 maxAmount;
         if (token == address(0)) {
-            // Calculate LTV for ETH deposits
-            amount = ILTVManager(ltvManager).calculateETHLTV();
+            // Get max allowed amount for ETH deposits
+            maxAmount = ILTVManager(ltvManager).calculateETHLTV();
         } else {
-            // Calculate LTV for LRT token deposits
-            amount = ILTVManager(ltvManager).calculateLRTLTV(token);
+            // Get max allowed amount for LRT token deposits
+            maxAmount = ILTVManager(ltvManager).calculateLRTLTV(token);
         }
+        require(amount <= maxAmount, "Amount exceeds LTV limit");
         _mint(to, amount);
     }
 
--- src/cover/CoverTokenFactory.sol
+++ src/cover/CoverTokenFactory.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+// In your factory contract:
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "./CoverToken.sol";  // Add this import
 
 contract CoverTokenFactory {
-
-    // TODO: mint token
-    // TODO: burn token
-    // TODO: set permissions
+    address immutable implementation;
+    
+    constructor(address _implementation) {
+        implementation = _implementation;
+    }
+    
+    function createCoverToken(address owner, address ltvManager) external returns (address) {
+        address clone = Clones.clone(implementation);
+        CoverToken(clone).initialize(owner, ltvManager);
+        return clone;
+    }
 }
