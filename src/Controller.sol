49bd6adc3f9e3d3f1e01a2298e3b0c0819ab98c7
kei at <arseneeth@pm.me>
2024-11-29 13:48:00 +0100

    wip

--- src/Controller.sol
+++ src/Controller.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {ICoverTokenFactory} from "./interfaces/ICoverTokenFactory.sol";
+import {ILTVManager} from "./interfaces/ILTVManager.sol";
+import {ICoverToken} from "./interfaces/ICoverToken.sol";
+import {IVault} from "./interfaces/IVault.sol";
+
 contract Controller {
+    /// @notice The vault contract instance
+    IVault public immutable vault;
+    
+    /// @notice The cover token factory contract instance
+    ICoverTokenFactory public immutable coverTokenFactory;
+    
+    /// @notice Mapping from base asset (ETH/LRT) to its cover token instance
+    mapping(address => address) public coverTokens;
 
-    constructor() {}
-
+    constructor(address _vault, address _coverTokenFactory) {
+        vault = IVault(_vault);
+        coverTokenFactory = ICoverTokenFactory(_coverTokenFactory);
+    }
 }
