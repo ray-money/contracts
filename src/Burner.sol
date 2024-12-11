3bbea1c2da63855d7df51976c55708d30f1fb6b2
kei at <arseneeth@pm.me>
2024-12-11 13:59:52 +0100

    upd claimCoverage

---
+++ src/Burner.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Controller} from "./Controller.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract Burner {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    Controller public immutable controller;
+    
+    // Set of supported assets for coverage
+    EnumerableSet.AddressSet private supportedAssets;
+
+    // Mapping from covered asset to cover token address
+    mapping(address => address) public coveredAssetToCoverToken;
+
+    // Base asset used for coverage
+    address public immutable baseAsset;
+    
+    constructor(address _controller) {
+        controller = Controller(_controller);
+        
+        // Get base asset from controller's vault
+        address vault = controller.vault();
+        baseAsset = controller.networkMiddleware().vaultConfigurator().getVaultConfig(vault).collateral;
+        
+        // Import supported assets from controller
+        uint256 numAssets = controller.supportedAssets().length;
+        for(uint256 i = 0; i < numAssets; i++) {
+            address asset = controller.supportedAssets(i);
+            supportedAssets.add(asset);
+            coveredAssetToCoverToken[asset] = controller.coveredAssetToCoverToken(asset);
+        }
+    }
+
+    /**
+     * @notice Claims coverage for a covered asset by burning cover tokens after slashing
+     * @param coveredAsset The address of the asset for which coverage is being claimed
+     * @param amount The amount of coverage to claim
+     * @dev Transfers covered asset and cover tokens from caller to this contract, sends base asset to caller
+     */
+    function claimCoverage(address coveredAsset, uint256 amount) external {
+        // Get cover token address for the covered asset
+        address coverToken = coveredAssetToCoverToken[coveredAsset];
+
+        // Transfer cover tokens from caller and burn them
+        IERC20(coverToken).transferFrom(msg.sender, address(this), amount);
+        IERC20(coverToken).burn(amount); // shouldn't work right now, we should arrange a role in CoverToken for that
+
+        // Transfer covered asset from caller
+        IERC20(coveredAsset).transferFrom(msg.sender, address(this), amount);
+
+        // Transfer base asset to caller
+        IERC20(baseAsset).transfer(msg.sender, amount);
+    }
+}
